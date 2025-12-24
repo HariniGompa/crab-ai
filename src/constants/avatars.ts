a5dd9c95b050852489980a4fcbae74058dab9487
Harini Gompa <harinigompa999@gmail.com>
2025-12-24 13:56:43 +0530

    Update avatars.ts

--- src/constants/avatars.ts
+++ src/constants/avatars.ts
@@ -4,11 +4,12 @@ export const AVATARS = [
   {
     id: "avatar_female_1",
     label: "Female Avatar",
-    src: "/avatars/female-1.png",
+    src: "/avatars/female-1.jpg",
   },
   {
     id: "avatar_male_1",
     label: "Male Avatar",
-    src: "/avatars/male-1.png",
+    src: "/avatars/male-1.jpg",
   },
 ];
+
